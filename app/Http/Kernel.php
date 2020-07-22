ed6be2b4fcbdfd2655fc0f120ae95b0b6f1ef5ec
Yoyo San <veselu@gmail.com>
2020-07-22 20:33:54 +0300

    fix(Http Kernel): Missing import

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 class Kernel extends HttpKernel
 {
