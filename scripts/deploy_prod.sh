5429ec3d29980c36f6b7c17dfa90bf22e10d39ec
Yoyo San <veselu@gmail.com>
2020-09-11 17:01:54 +0300

    feat: Add deploy script and task for VS code

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 git pull
+# step 1
+php artisan down
 composer install --optimize-autoloader --no-dev
-php artisan migrate
+# php artisan migrate
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
@@ -10,3 +12,6 @@ php artisan view:cache
 # fix permissions
 chown -R mpalade:mpalade .
 chmod -R u+rwX,go+rX,go-w .
+
+# all done
+php artisan up
