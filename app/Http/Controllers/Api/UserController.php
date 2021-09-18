3b8fd2983a707ff359fabb61efb579c7a8084a9a
Marius Palade <contact@mpalade.ro>
2021-09-18 08:09:59 +0300

    refactor to use controller actions instead of closure routes

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+
+class UserController extends Controller
+{
+    /**
+     * @param  Request  $request
+     * @return mixed
+     */
+    public function info(Request $request)
+    {
+        return $request->user();
+    }
+}
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,6 +13,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('welcome');
     }
 }
