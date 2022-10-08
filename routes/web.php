79b8e592250e652f0696ec8e0f86b76806c2d3e0
Marius Palade <marius.palade@qualitance.com>
2022-10-08 16:11:48 +0300

    fix(routes): home route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'HomeController@index');
+Route::get('/', [HomeController::class, 'index']);
